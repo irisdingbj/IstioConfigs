30bc1f0ed1858144234c1217f9782208d4d389cc
irisdingbj <shaojun.ding@intel.com>
2024-05-29 16:35:11 -0700

    n16

--- microservices-connector/cmd/main.go
+++ microservices-connector/cmd/main.go
@@ -30,7 +30,7 @@ import (
 
 var (
 	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("s889u9u88787ut8888yy7auuaaaaaassssssssssss sssssstup ")
+	setupLog = ctrl.Log.WithName("s8u9u88787ut8888yy7auuaaaaaassssssssssss sssssstup ")
 u)
 
 func init() {
